a0f0959c6036e5e375067d55171d2e91b5ef9572
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-17 17:02:56 -0600

    Redireccion al Iniciar Sesion

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Rutas de vacantes
+
+Route::get('/vacantes','VacanteController@index')->name('vacantes.index');
