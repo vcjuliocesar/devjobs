f33ae982b23a7f47a2b97f5c1b63f3db491924ed
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-17 16:01:16 -0600

    Tailwind a la pantalla de verificacion

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <div class="container mx-auto mt-20 text-center">
+        <div class="text-2xl my-5">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
+        @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('A fresh verification link has been sent to your email address.') }}
             </div>
-        </div>
+        @endif
+
+        <p>{{ __('Before proceeding, please check your email for a verification link.') }}</p>
+        <p class="mt-5">{{ __('If you did not receive the email') }}</p>
+        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <button type="submit"
+                class="mt-10 max-w-sm bg-teal-500 w-full hover_bg-teal-700 text-gray-100 p-3 focus:outlone-none focus:shadow-outlone uppercase font-bold">{{ __('click here to request another') }}</button>
+        </form>
+
     </div>
-</div>
 @endsection
