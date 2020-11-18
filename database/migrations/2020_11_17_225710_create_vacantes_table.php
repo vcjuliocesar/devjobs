a9c08d3ab254700dc9e8093cfa22689969116631
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-18 12:35:01 -0600

    Creando la tabla de vacantes y categorias

--- database/migrations/2020_11_17_225710_create_vacantes_table.php
+++ database/migrations/2020_11_17_225710_create_vacantes_table.php
@@ -13,8 +13,15 @@ class CreateVacantesTable extends Migration
      */
     public function up()
     {
+        Schema::create('categorias',function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('categoria_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,5 +34,6 @@ class CreateVacantesTable extends Migration
     public function down()
     {
         Schema::dropIfExists('vacantes');
+        Schema::dropIfExists('categorias');
     }
 }
