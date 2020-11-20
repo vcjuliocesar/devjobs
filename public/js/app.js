a5db98171c6068c03ec0352440e8cf3ef7208b4c
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-20 17:31:21 -0600

    Marcando o Desmarcando las Habilidades

--- public/js/app.js
+++ public/js/app.js
@@ -1908,6 +1908,20 @@ module.exports = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+//
+//
 //
 //
 //
@@ -1921,9 +1935,34 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
-  props: ['skills'],
+  props: ["skills"],
   mounted: function mounted() {
     console.log(this.skills);
+  },
+  data: function data() {
+    return {
+      habilidades: new Set()
+    };
+  },
+  methods: {
+    activar: function activar(e) {
+      if (e.target.classList.contains("bg-teal-400")) {
+        //el skill esta activo
+        e.target.classList.remove("bg-teal-400"); //Eliminar del set de habilidades
+
+        this.habilidades["delete"](e.target.textContent);
+      } else {
+        //No esta activo, añadirlo
+        e.target.classList.add("bg-teal-400"); //Agregar al set de habilidades
+
+        this.habilidades.add(e.target.textContent);
+      } // agregar las habilidades al input hidden
+
+
+      var stringHabilidades = _toConsumableArray(this.habilidades);
+
+      document.querySelector('#skills').value = stringHabilidades;
+    }
   }
 });
 
@@ -37519,21 +37558,30 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c(
-    "ul",
-    { staticClass: "flex flex-wrap justify-center" },
-    _vm._l(this.skills, function(skill, i) {
-      return _c(
-        "li",
-        {
-          key: i,
-          staticClass: "border border-gray-500 px-10 py-3 mb-3 rounded mr-4"
-        },
-        [_vm._v("\n        " + _vm._s(skill) + "\n    ")]
-      )
-    }),
-    0
-  )
+  return _c("div", [
+    _c(
+      "ul",
+      { staticClass: "flex flex-wrap justify-center" },
+      _vm._l(this.skills, function(skill, i) {
+        return _c(
+          "li",
+          {
+            key: i,
+            staticClass: "border border-gray-500 px-10 py-3 mb-3 rounded mr-4",
+            on: {
+              click: function($event) {
+                return _vm.activar($event)
+              }
+            }
+          },
+          [_vm._v(_vm._s(skill))]
+        )
+      }),
+      0
+    ),
+    _vm._v(" "),
+    _c("input", { attrs: { type: "hidden", name: "skills", id: "skills" } })
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/components/ListaSkills.vue
+++ resources/js/components/ListaSkills.vue
@@ -1,20 +1,46 @@
 <template>
-    <ul class="flex flex-wrap justify-center">
-        <li
-            class="border border-gray-500 px-10 py-3 mb-3 rounded mr-4"
-            v-for="(skill,i) in this.skills"
-            v-bind:key="i"
-        >
-            {{skill}}
-        </li>
-    </ul>
+    <div>
+        <ul class="flex flex-wrap justify-center">
+            <li
+                class="border border-gray-500 px-10 py-3 mb-3 rounded mr-4"
+                v-for="(skill, i) in this.skills"
+                v-bind:key="i"
+                v-on:click="activar($event)"
+            >{{ skill }}</li>
+        </ul>
+        <input type="hidden" name="skills" id="skills" />
+    </div>
 </template>
 
 <script>
 export default {
-    props:['skills'],
-    mounted(){
+    props: ["skills"],
+    mounted() {
         console.log(this.skills);
+    },
+    data: function() {
+        return {
+            habilidades: new Set()
+        };
+    },
+    methods: {
+        activar(e) {
+            if (e.target.classList.contains("bg-teal-400")) {
+                //el skill esta activo
+                e.target.classList.remove("bg-teal-400");
+                //Eliminar del set de habilidades
+                this.habilidades.delete(e.target.textContent);
+            } else {
+                //No esta activo, añadirlo
+                e.target.classList.add("bg-teal-400");
+                //Agregar al set de habilidades
+                this.habilidades.add(e.target.textContent);
+            }
+
+            // agregar las habilidades al input hidden
+            const stringHabilidades = [...this.habilidades];
+            document.querySelector('#skills').value = stringHabilidades;
+        }
     }
-}
+};
 </script>
