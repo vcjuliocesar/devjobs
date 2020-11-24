f03127f86ef2dcaebcfbd87a30cbd42f743c4ee5
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-24 09:24:13 -0600

    Mostrando una vacante como Activa o Inactiva

--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -6,6 +6,8 @@
 
 @section('content')
 <h1 class="text-2xl text-center mt-10">Administrar Vacantes</h1>
+@if (count($vacantes) > 0)
+
 
 <div class="flex flex-col mt-10">
     <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
@@ -40,8 +42,10 @@
                 </div>
               </td>
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full">
-
+                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full
+                    {{$vacante->activa ? 'bg-green-100 text-green-800' :'bg-red-100 text-red-800'}}
+                ">
+                    {{$vacante->activa ? 'Activa':'Inactiva'}}
                 </span>
               </td>
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
@@ -65,4 +69,8 @@
     </div>
   </div>
   {{$vacantes->links()}}
+
+  @else
+    <p class="text-center mt-10">No tienes vacantes aún</p>
+  @endif
 @endsection
