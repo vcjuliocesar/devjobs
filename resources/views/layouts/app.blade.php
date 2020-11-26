4645bc2a68bd2e87c4859ee13988b5aa5c2af6ab
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-26 12:50:13 -0600

    Mostrar un Numero con el total de notificaciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,20 +14,24 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+    @yield('styles')
 
     <!-- Styles -->
-    @yield('styles')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gray-200 min-h-screen leading-none">
-    @if (session('estado'))
+
+    @if(session('estado'))
         <div class="bg-teal-500 p-8 text-center text-white font-bold uppercase">
-            {{session('estado')}}
+            {{ session('estado') }}
         </div>
     @endif
+
+
     <div id="app">
-        <nav class="bg-gray-800 shadow-md py-2">
+        <nav class="bg-gray-800 shadow-md py-6">
             <div class="container mx-auto md:px-0">
                 <div class="flex items-center justify-around">
                     <a class="text-2xl text-white" href="{{ url('/') }}">
@@ -35,40 +39,45 @@
                     </a>
 
                     <nav class="flex-1 text-right">
-                            <!-- Authentication Links -->
                             @guest
-                                    <a class="text-white no-underline hover:underline hover:text-gray-300 p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="text-white no-underline hover:underline hover:text-gray-300 p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 @if (Route::has('register'))
                                     <a class="text-white no-underline hover:underline hover:text-gray-300 p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             @else
-                                   <span class="text-gray-300 text-sm pr-4"> {{ Auth::user()->name }}</span>
+                                    <span class="text-gray-300 text-sm pr-4"> {{ Auth::user()->name }}  </span>
 
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
+                                    <a
+                                        href=""
+                                        class="bg-teal-500 rounded-full mr-2 px-3 py-1 font-bold text-sm text-white"
+                                    > {{ Auth::user()->unreadNotifications->count() }} </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
+                                    <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
                             @endguest
-                    </nav>
+                        </nav>
                 </div>
             </div>
         </nav>
+
         <div class="bg-gray-700">
-            <nav class="container mx-auto flex space-x-1">
+            <nav class="container mx-auto flex flex-col text-center md:flex-row  space-x-1">
                 @yield('navegacion')
             </nav>
         </div>
+
         <main class="mt-10 container mx-auto">
             @yield('content')
         </main>
     </div>
+
     @yield('scripts')
 </body>
 </html>
