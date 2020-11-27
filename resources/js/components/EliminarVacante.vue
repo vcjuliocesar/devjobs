f1cd5a53d15cf50545cf14974eafb9f410ab641a
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-27 12:51:21 -0600

    Mostrando sweetalert al dar click

--- resources/js/components/EliminarVacante.vue
+++ resources/js/components/EliminarVacante.vue
@@ -1,16 +1,37 @@
 <template>
-    <a href="#" class="text-red-600 hover:text-red-900  mr-5"
+    <a
+        href="#"
+        class="text-red-600 hover:text-red-900  mr-5"
         @click="EliminarVacante"
-    >Eliminar</a>
+        >Eliminar</a
+    >
 </template>
 
 <script>
-export default{
-    props:['vacanteId'],
-    methods:{
+export default {
+    props: ["vacanteId"],
+    methods: {
         EliminarVacante() {
-            console.log('Eliminando...',this.vacanteId)
+            //console.log("Eliminando...", this.vacanteId);
+            this.$swal.fire({
+                title: "¿Deseas eliminar esta vacante?",
+                text: "Una vez eliminada no se puede recuperar!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Si",
+                cancelButtonText:'No'
+            }).then(result => {
+                if (result.isConfirmed) {
+                    this.$swal.fire(
+                        "Vacante Eliminada",
+                        "Vacante eliminada correctamente",
+                        "success"
+                    );
+                }
+            });
         }
     }
-}
+};
 </script>
