29394cb7a6e35b2936938d812b8554571de3ffe0
vcjuliocesar <vcjuliocesar@outlook.com>
2020-11-27 12:37:06 -0600

    Registrando el Componente para eliminar vacantes

---
+++ resources/js/components/EliminarVacante.vue
@@ -0,0 +1,16 @@
+<template>
+    <a href="#" class="text-red-600 hover:text-red-900  mr-5"
+        @click="EliminarVacante"
+    >Eliminar</a>
+</template>
+
+<script>
+export default{
+    props:['vacanteId'],
+    methods:{
+        EliminarVacante() {
+            console.log('Eliminando...',this.vacanteId)
+        }
+    }
+}
+</script>
