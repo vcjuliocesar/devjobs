903b5e38f53e00b2b51d773675a3be9c077ace56
vcjuliocesar <vcjuliocesar@outlook.com>
2020-12-01 20:49:35 -0600

    filtrando vacantes por categoria

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Vacante;
+use App\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function show(Categoria $categoria)
+    {
+        $vacantes = Vacante::where('categoria_id', $categoria->id)->where('activa', true)->paginate(10);
+
+        // dd($vacantes);
+
+        return view('categorias.show', compact('vacantes', 'categoria'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 //Vista de inicio
 Route::get('/','InicioController')->name('inicio');
 
+//Categorias
+Route::get('/categorias/{categoria}','CategoriaController@show')->name('categorias.show');
+
 //Enviar datos para una vacante
 Route::get('/candidatos/{id}','CandidatoController@index')->name('candidatos.index');
 Route::post('/candidatos','CandidatoController@store')->name('candidatos.store');
